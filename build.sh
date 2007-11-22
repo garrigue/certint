fa8f9bc9f7e83824ffb1b5ac89c4f86301e1d83d
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2007-11-22 00:48:59 +0000

    build proof

---
+++ build.sh
@@ -0,0 +1,4 @@
+coqc Lib_Tactic.v Lib_ListFacts.v Lib_ListFactsMore.v
+coqc Lib_MyFSetInterface.v Lib_FinSet.v Lib_FinSetImpl.v
+coqc Metatheory_Var.v Metatheory_Fresh.v Metatheory_Env.v Metatheory.v
+coqc ML_SP_Definitions.v ML_SP_Infrastructure.v ML_SP_Soundness.v ML_SP_Domain.v
