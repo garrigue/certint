2aea6bf03f08ae6f3b41c1823e0de22b20e935f1
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2008-11-21 08:54:25 +0000

    doc

--- use_typinf.ml
+++ use_typinf.ml
@@ -31,13 +31,23 @@ let typinf1 trm =
 
 open Typinf.Cstr;;
 
+(* First example: (Coq_tag A) is a function constant, which takes any value
+   and returns a polymorphic variant A with this value as argument *)
+(* This example is equivalent to the ocaml term [fun x -> `A0 x] *)
 typinf1 (Coq_trm_cst (Const.Coq_tag (Variables.var_of_nat O)));;
 
 let rec nat_of_int n = if n <= 0 then O else S (nat_of_int (n-1));;
 
+(* Second example: (Coq_matches [A1; ..; An]) is a n+1-ary function constant
+   which takes n functions and a polymorphic variant as argument, and
+   (fi v) if the argument was (Ai v). *)
+(* This example is equivalent to [function `A20 x -> x | `A21 x -> x] *)
 let trm =
-  Coq_trm_app (Coq_trm_cst
-                 (Const.Coq_matches
-                    (Cons (Variables.var_of_nat (nat_of_int 5), Nil))),
-               Coq_trm_abs (Coq_trm_bvar O));;
+  Coq_trm_app (Coq_trm_app (
+    Coq_trm_cst
+      (Const.Coq_matches
+         (Cons (Variables.var_of_nat (nat_of_int 20),
+                Cons (Variables.var_of_nat (nat_of_int 21), Nil)))),
+    Coq_trm_abs (Coq_trm_bvar O)),
+ Coq_trm_abs (Coq_trm_bvar O));;
 typinf1 trm;;
