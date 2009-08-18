5c0b44eee8538df1f7710d3a177d81748f6e7796
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2009-08-18 02:21:23 +0000

    shorten proofs

--- Extraction.v
+++ Extraction.v
@@ -29,23 +29,16 @@ Definition decidable (A : Set) (P : A -> Prop) :=
   forall x, {P x} + {~P x}.
 
 Definition ok_dec : decidable (@ok sch).
-  intro E; induction E.
-    left. apply ok_empty.
+  intro E; induction E; env_fix.
+    auto.
+  destruct a; env_fix.
   destruct IHE.
-    destruct a.
     case_eq (get v E); intros.
-      right.
-      intro.
-      replace ((v,s)::E) with (E & v ~ s) in H0 by simpl*.
-      inversions H0.
-      elim (H5 (binds_dom H)).
-    left.
-    apply (@ok_push _ E v s o).
-    apply (get_none_notin  _ H).
-  right.
-  destruct a.
-  change (~ok (E & v ~ s)).
-  intro.
+      right; intro.
+      elim (binds_fresh H).
+      inversions* H0.
+    auto.
+  right; intro.
   elim n.
   inversion* H.
 Defined.
@@ -58,33 +51,27 @@ Inductive type_n (n:nat) : typ -> Prop :=
 Hint Constructors type_n.
 
 Definition type_n_dec : forall n , decidable (type_n n).
-  introv T; induction T.
-    destruct (le_lt_dec n n0).
-      right. intro. inversions H. omega.
-    left*.
-   left*.
+  introv T; induction* T.
+    destruct* (le_lt_dec n n0).
+    right; intro. inversions H. omega.
   destruct IHT1.
-    destruct IHT2.
-      left*.
+    destruct* IHT2.
     right; intro. inversions* H.
   right; intro. inversions* H.
 Defined.
 
-Lemma type_n_typ_body : forall n T Xs,
-  n = length Xs -> 
-  (type_n n T <-> type (typ_open_vars T Xs)).
+Lemma type_n_typ_body : forall T Xs,
+  (type_n (length Xs) T <-> type (typ_open_vars T Xs)).
 Proof.
   unfold typ_open_vars.
   intros; split.
     induction 1; simpl*.
-  induction T; simpl; intros.
-      destruct* (le_lt_dec n n0).
-      rewrite H in l.
-      unfold typ_fvars in H0.
-      rewrite <- (map_length typ_fvar) in l.
-      rewrite (nth_overflow _ _ l) in H0. inversion H0.
-    auto.
-  inversions* H0.
+  induction T; simpl*; intros.
+    destruct* (le_lt_dec (length Xs) n).
+    unfold typ_fvars in H.
+    rewrite <- (map_length typ_fvar) in l.
+    rewrite (nth_overflow _ _ l) in H. inversion H.
+  inversions* H.
 Qed.
 
 Definition list_forall_dec : forall (A:Set) (P:A->Prop),
@@ -104,36 +91,32 @@ Definition scheme_dec : decidable scheme.
     unfold All_kind_types.
     puts (list_forall_dec (fun k => H (kind_types k))).
     destruct (H0 Ks).
-      left; intuition. apply* (proj1 (type_n_typ_body T _ H1)).
-      apply* list_forall_imp; intros. simpl in H2.
+      left; intuition; subst. apply* (proj1 (type_n_typ_body T Xs)).
+      apply* list_forall_imp; intros. simpl in H1.
       apply* list_forall_imp; intros.
-      apply* (proj1 (type_n_typ_body x0 _ H1)).
+      apply* (proj1 (type_n_typ_body x0 Xs)).
     right; intro.
     elim n0; clear -H1.
     destruct (var_freshes {} n).
     destruct* (H1 x); clear H1.
     apply* list_forall_imp; intros. simpl in H1.
     refine (list_forall_imp _ _ H1); intros.
-    refine (proj2 (type_n_typ_body x1 _ _) H2); auto*.
+    rewrite (fresh_length _ _ _ f).
+    refine (proj2 (type_n_typ_body x1 x) H2); auto*.
   right; intro.
   elim n0; clear -H.
   destruct (var_freshes {} n).
   destruct* (H x); clear H.
-  refine (proj2 (type_n_typ_body T _ _) H0); auto*.
+  rewrite (fresh_length _ _ _ f).
+  refine (proj2 (type_n_typ_body T x) H0); auto*.
 Defined.
 
 Definition env_prop_dec : forall (A:Set) (P:A->Prop),
   decidable P -> decidable (env_prop P).
   introv HP E; induction E.
     left; intro; intros. elim H.
-  destruct a.
+  destruct a; env_fix.
   destruct* (HP a).
-  destruct IHE.
-    left; intro; simpl; intros.
-    destruct* H.
-    inversions* H.
-  right; intro. elim n.
-  intro; auto*.
 Defined.
 
 Lemma env_weaker_refl : forall E, Rename2.env_weaker E E.
@@ -154,19 +137,15 @@ Definition typinf1 : forall E t,
     clear H; destruct (ok_dec E).
       destruct (env_prop_dec scheme_dec E).
         case_eq (typinf' E t0); intros.
-          destruct p.
-          left.
-          exists (e0,t1).
-          apply* typinf_sound'. split*.
+          left; exists p. destruct p.
+          apply* typinf_sound'.
         right; right; introv Typ.
         destruct* (Rename2.typing_remove_gc Typ {} (@env_weaker_refl E))
           as [K' [HK' Typ']].
         destruct* (typinf_principal' Hempty Typ') as [K0 [T' [TI]]].
         rewrite H in TI; discriminate.
-      right; right; introv Typ.
-      destruct* (proj42 (typing_regular Typ)).
-    right; right; introv Typ.
-    destruct* (proj42 (typing_regular Typ)).
+      right; right; introv Typ. elim n; auto.
+    right; right; introv Typ. elim n; auto.
   right; left.
   intro. rewrite H0 in H.
   puts (S.is_empty_1 (S.empty_1)).
